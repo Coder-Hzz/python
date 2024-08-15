bc0356de150764ee31b442e3e6d6979de8006163
Coder-Hzz <2677455638@qq.com>
2024-08-15 17:19:02 +0800

    Update test2.py

--- practice2/test2.py
+++ practice2/test2.py
@@ -1,1 +1,21 @@
+lst=[]
+s=input('请输入字符串:')
+for i in range(len(s)):
+    for j in range(i+1,len(s)+1):
+        lst.append(s[i:j])
+
+lst2=[]
+for item in lst:
+    lst1=[]
+    i = 0
+    while i < len(item):
+        if item[i] not in lst1:
+            lst1.append(item[i])
+            i+=1
+        else:
+            break
+    lst2.append(''.join(lst1))
+
+# print(lst2)
+print(len(max(lst2,key=len)))
 
